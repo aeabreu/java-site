8532e834bf41dc9a3f34aa8769193059562c5071
Peter Tubig <peter.tubig@samba.tv>
2017-03-19 18:38:07 -0700

    Add aeabeu.js

---
+++ aeabeu.js
@@ -0,0 +1,49 @@
+$(function() {
+//          function Alex() {
+//            this.name = 'alex';
+//            this.lastName = 'abreu';
+//          }
+//
+//          Alex.prototype.getName = function() {
+//            return 'whaddddup';
+//          }
+//
+//
+//          var peter = new Alex();
+//          console.log(peter.getName());
+
+  function updateGreeting() {
+    var MORNING = 'morning';
+    var AFTERNOON = 'afternoon';
+    var NIGHT = 'night';
+
+    var currentDay;
+    var timeOfDay = $('#timeOfDay');
+
+    var date = new Date();
+    if (date.getHours() < 12) {
+      currentDay = MORNING;
+    } else if (date.getHours() < 18) {
+      currentDay = AFTERNOON;
+    } else {
+      currentDay = NIGHT;
+    }
+
+    timeOfDay.html(currentDay + ', the time is ' + date);
+  }
+
+  updateGreeting();
+
+  $('#skillSummaryButton').click(function() {
+    if ($('#skillSummary').hasClass('open')) {
+      $('#skillSummary').removeClass('open');
+      $('#skillSummary').addClass('close');
+      $('#skillSummaryButton').text('Open');
+    } else {
+      $('#skillSummary').removeClass('close');
+      $('#skillSummary').addClass('open');
+      $('#skillSummaryButton').text('Close');
+    }
+  });
+
+});
